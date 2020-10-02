bfca39374cf55b9a49c95cff298279f78864e6a9
Andrew Barwise <36126079+andrewbarwise@users.noreply.github.com>
2020-10-02 14:12:07 +0800

    deleted redundent code

--- seq.py
+++ seq.py
@@ -1,43 +1,3 @@
-"""# Import libraries and modules
-from Bio import Align
-import pandas as pd
-
-# create a  pairwiseAligner object
-aligner = Align.PairwiseAligner()
-
-# set alignment parameters
-aligner.match_score = 1.0
-aligner.mismatch_score = -2.0
-aligner.open_gap_score = -2.5
-aligner.gap_score = -2.0
-
-# read in the data
-df = pd.read_csv('all.tsv', header=None, delimiter=r"\s+")
-lst = df.iloc[1:, 0].tolist()
-
-
-ii = 0
-while ii+1 < len(lst):
-    X = lst[ii]
-    Y = lst[ii + 1]
-    
-    #obtain the score
-    score = aligner.score(X,Y)
-    
-    # obtain the the variable of greatest length
-    denom = max(len(X),len(Y))
-    
-    # obtain a % of similarity
-    adjustedScore = score / denom 
-
-    #alignments = aligner.align(X,Y)
-
-    if adjustedScore >= 0.99:
-        print(X," : ", Y, '=',adjustedScore)
-        print(len(lst))
-    ii = ii + 1"""
-
-#######################################################
 from Bio import Align
 import pandas as pd
 
@@ -64,13 +24,12 @@ def param(lst, xx, yy, zz, qq, cc):
         # obtain a % of similarity
         adjustedScore = score / denom 
 
-        #alignments = aligner.align(X,Y)
-        newLst = []
+        
         if adjustedScore >= cc:
-            newLst.append(adjustedScore)
+            print(X, ":", Y, '=', adjustedScore)
 
         ii = ii + 1
-    return newLst
+        
 
 def main(inputFile):
     # read in the data
@@ -84,9 +43,8 @@ def main(inputFile):
 
     cc = float(input("Please input the cut off percentage: "))
 
-    output = param(lst, xx, yy, zz, qq, cc)
-    print(output)
-
+    param(lst, xx, yy, zz, qq, cc)
+    
 if __name__ == '__main__':
     main('all.tsv')
 
