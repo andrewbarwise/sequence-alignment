0d9b3431707e5ea9508a976cad52c7a40dcd6446
Andrew Barwise <36126079+andrewbarwise@users.noreply.github.com>
2020-10-14 09:28:52 +0800

    update

--- seq.py
+++ seq.py
@@ -2,15 +2,6 @@ from Bio import Align
 import pandas as pd
 import os
 
-def param(lst, xx, yy, zz, qq, cc):
-    # create the aligner object
-    aligner = Align.PairwiseAligner()
-
-    aligner.match_score = xx
-    aligner.mismatch_score = yy
-    aligner.open_gap_score = zz
-    aligner.gap_score = qq
-
 def alignment(cdr3_1, cdr3_2):
     aligner = Align.PairwiseAligner()
     score = aligner.score(cdr3_1,cdr3_2)
@@ -45,27 +36,12 @@ def main(inputFile):
         print("The incorrect file has been entered")    
         return
 
-def main(inputFile):
-    # read in the data
-    df = pd.read_csv(inputFile, header=None, delimiter=r"\s+")
-    lst1 = df.iloc[1:, 0].tolist()
-    lst2 = df.iloc[1:, 3].tolist()
-    
-    # create a dataframe
-    lst = pd.DataFrame(
-        {'CDR3':lst1,
-        'patient':lst2}
-    )
-
     xx = float(input("Please enter the score for a matching character: "))
     yy = float(input("Please enter the penalty for mismatched characters: ")) * - 1
     zz = float(input("Please enter the penalty for the opening of a gap: ")) * - 1
     qq = float(input("Please enter the penalty for the continuation of a gap: ")) * - 1
 
     cc = float(input("Please input the cut off percentage: "))
-
-    param(lst, xx, yy, zz, qq, cc)
-    
 
     input("Press enter to exit ;)")
     
