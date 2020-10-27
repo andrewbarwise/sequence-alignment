c34af559d4e1b63dcf9249875705cb8536b8f3a6
Andrew Barwise <36126079+andrewbarwise@users.noreply.github.com>
2020-10-27 11:09:48 +0800

    Update seq.py

--- seq.py
+++ seq.py
@@ -1,16 +1,11 @@
 from Bio import Align
 import pandas as pd
 import os
+from scipy.cluster.hierarchy import dendrogram, linkage
+import numpy as np
+from matplotlib import pyplot as plt
 
-def param(lst, xx, yy, zz, qq, cc):
-    # create the aligner object
-    aligner = Align.PairwiseAligner()
-
-    aligner.match_score = xx
-    aligner.mismatch_score = yy
-    aligner.open_gap_score = zz
-    aligner.gap_score = qq
-
+# this function computes a pairwise alignment score
 def alignment(cdr3_1, cdr3_2):
     aligner = Align.PairwiseAligner()
     score = aligner.score(cdr3_1,cdr3_2)
@@ -18,56 +13,63 @@ def alignment(cdr3_1, cdr3_2):
     adjustedScore = score / denom
     return adjustedScore
 
-def similarity():
-    similarity_df = pd.DataFrame()
-    # for each line compare the CDR3
-    for index_1, row_1 in lst.iterrows():
-        for index_2, row_2 in lst.iterrows():
-            similarity_score = alignment(data.at[index_1,'CDR3'], data.at[index_2,'CDR3'])
-            similarity_df.loc[data.at[index_1,'CDR3'],data.at[index_2,'CDR3']]
+# creates a data frame to hold the observations 
+def createDF(inputFile):
+    df = pd.read_csv('all.tsv', header=None, delimiter=r"\s+")
+    lst1 = df.iloc[1:40, 0].tolist()
+    lst2 = df.iloc[1:40, 3].tolist()
 
-    return similarity_df
-
-def fileInput();
-    if os.path.isfile(inputFile):
-        # read in the data
-        df = pd.read_csv(inputFile, header=None, delimiter=r"\s+")
-        lst1 = df.iloc[1:, 0].tolist()
-        lst2 = df.iloc[1:, 3].tolist()
-        
-        # create a dataframe
-        lst = pd.DataFrame(
-            {'CDR3':lst1,
-            'patient':lst2}
-        )
-    else:
-        print("The incorect file has been entered")    
-        return
-
-def main(inputFile):
-    # read in the data
-    df = pd.read_csv(inputFile, header=None, delimiter=r"\s+")
-    lst1 = df.iloc[1:, 0].tolist()
-    lst2 = df.iloc[1:, 3].tolist()
-    
-    # create a dataframe
     lst = pd.DataFrame(
         {'CDR3':lst1,
         'patient':lst2}
     )
+    # create a new column and remove first 4 characters from CDR3 string for computation
+    lst['CDR3_Alt'] = lst['CDR3'].str[4:]
+    return lst
 
-    xx = float(input("Please enter the score for a matching character: "))
-    yy = float(input("Please enter the penalty for mismatched characters: ")) * - 1
-    zz = float(input("Please enter the penalty for the opening of a gap: ")) * - 1
-    qq = float(input("Please enter the penalty for the continuation of a gap: ")) * - 1
-
-    cc = float(input("Please input the cut off percentage: "))
-
-    param(lst, xx, yy, zz, qq, cc)
+# create a similarity matrix and fill with the pairwise scores
+def simMat(inputFile):
+    # create empty similarity matrix
+    similarity_df = pd.DataFrame()
     
+    # call createDF()
+    lst = createDF(inputFile)
+    
+    # for each line compare the CDR3
+    for index_1, row_1 in lst.iterrows():
+        for index_2, row_2 in lst.iterrows():
+            similarity_score = alignment(lst.at[index_1,'CDR3_Alt'], lst.at[index_2,'CDR3_Alt'])
+            similarity_df.loc[lst.at[index_1,'CDR3'],lst.at[index_2,'CDR3']] = similarity_score
 
+    print(similarity_df)
+    linked = linkage(similarity_df, 'ward')
+    return linked
+
+# plot a dendogram using the similarity matrix
+def plotDendo(inputFile):
+    linked = simMat(inputFile)
+    plt.figure(figsize=(15, 12))
+    plt.title("Sequence Alignment Dendogram")
+    dendrogram(
+                linked,
+                truncate_mode= 'level',
+                orientation='right',
+                # need to add labels to the dendogram
+                distance_sort='descending',
+                show_leaf_counts=False
+            )
+    plt.show()
+
+# compute similarity scores
+def simScore():
+
+
+
+def main(inputFile):
+    createDF(inputFile)
+    simMat(inputFile)
+    plotDendo(inputFile)
     input("Press enter to exit ;)")
     
-if __name__ == '__main__':
+if __name__ == '__main__':   
     main('all.tsv')
-
